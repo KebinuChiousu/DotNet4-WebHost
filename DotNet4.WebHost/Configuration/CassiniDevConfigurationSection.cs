61391c90b6641532d6650ca1a55b390bda8dab19
Kevin Meredith kevmer <kevin@meredithkm.info>
2022-05-11 20:36:19 -0400

    Created Microsoft.NET.SDK project for Cassini Web Host and set target to .net472

---
+++ DotNet4.WebHost/Configuration/CassiniDevConfigurationSection.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+
+namespace DotNet4.WebHost.Configuration
+{
+    ///<summary>
+    ///</summary>
+    public class CassiniDevConfigurationSection : ConfigurationSection
+    {
+
+        ///<summary>
+        ///</summary>
+        public static CassiniDevConfigurationSection Instance
+        {
+            get
+            {
+                return (CassiniDevConfigurationSection)ConfigurationManager.GetSection("cassinidev");
+            }
+        }
+
+        ///<summary>
+        ///</summary>
+        [ConfigurationProperty("profiles")]
+        public CassiniDevProfileElementCollection Profiles
+        {
+            get
+            {
+                return (CassiniDevProfileElementCollection)this["profiles"];
+            }
+        }
+    }
+}
